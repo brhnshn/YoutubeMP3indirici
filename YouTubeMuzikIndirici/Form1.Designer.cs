83446ae95ef251bcf330834e72e8ecfeffe0608b
brhnshn <sahinburhan501@gmail.com>
2026-02-18 20:43:40 +0300

    Add project files.

---
+++ YouTubeMuzikIndirici/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace YouTubeMuzikIndirici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnIndir = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.chkSor = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(202, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube Linki";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtUrl.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUrl.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUrl.Location = new System.Drawing.Point(132, 153);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(314, 23);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "Youtube Linki";
+            // 
+            // btnIndir
+            // 
+            this.btnIndir.BackColor = System.Drawing.Color.Red;
+            this.btnIndir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIndir.FlatAppearance.BorderSize = 0;
+            this.btnIndir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIndir.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnIndir.ForeColor = System.Drawing.Color.White;
+            this.btnIndir.Location = new System.Drawing.Point(207, 318);
+            this.btnIndir.Name = "btnIndir";
+            this.btnIndir.Size = new System.Drawing.Size(122, 47);
+            this.btnIndir.TabIndex = 2;
+            this.btnIndir.Text = "İNDİR";
+            this.btnIndir.UseVisualStyleBackColor = false;
+            this.btnIndir.Click += new System.EventHandler(this.btnIndir_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(132, 215);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(314, 30);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(203, 382);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(0, 23);
+            this.lblDurum.TabIndex = 4;
+            // 
+            // chkSor
+            // 
+            this.chkSor.AutoSize = true;
+            this.chkSor.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkSor.Location = new System.Drawing.Point(183, 276);
+            this.chkSor.Name = "chkSor";
+            this.chkSor.Size = new System.Drawing.Size(208, 24);
+            this.chkSor.TabIndex = 5;
+            this.chkSor.Text = "Kaydedilecek yeri bana sor";
+            this.chkSor.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(561, 469);
+            this.Controls.Add(this.chkSor);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnIndir);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Youtube MP3 İndirici";
+            this.Load += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnIndir;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.CheckBox chkSor;
+    }
+}
+
---
+++ YouTubeMuzikIndirici/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Diagnostics; // Dosya ve klasör açmak için şart
+using System.IO;          // Dosya işlemleri için
+using System.Linq;
+using System.Windows.Forms;
+using YoutubeExplode;     // YouTube kütüphanesi
+using YoutubeExplode.Videos.Streams;
+
+namespace YouTubeMuzikIndirici
+{
+    public partial class Form1 : Form
+    {
+        // YouTube istemcisini başta oluşturmuyoruz (Hızlı açılış için)
+        private YoutubeClient youtube;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Form Yüklenirken (Sadece temel ayarlar)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+
+            // Eğer tasarımda "chkSor" (Bana Sor kutusu) varsa işaretini kaldır
+            if (Controls.Find("chkSor", true).Length > 0 && chkSor != null)
+                chkSor.Checked = false;
+        }
+
+        // Form Ekrana Geldikten Sonra (Pano kontrolü burada yapılırsa program donmaz)
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Clipboard.ContainsText())
+                {
+                    string gelenYazi = Clipboard.GetText();
+                    // Link gerçekten YouTube linki mi?
+                    if (!string.IsNullOrWhiteSpace(gelenYazi) &&
+                       (gelenYazi.Contains("youtube.com") || gelenYazi.Contains("youtu.be")))
+                    {
+                        txtUrl.Text = gelenYazi;
+                        // İmleci sona al
+                        txtUrl.SelectionStart = txtUrl.Text.Length;
+                    }
+                }
+            }
+            catch { /* Pano hatası olursa görmezden gel */ }
+        }
+
+        // --- İNDİR BUTONU ---
+        private async void btnIndir_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+
+            // 1. Link Kontrolü
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                MessageBox.Show("Lütfen geçerli bir YouTube linki yapıştırın.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Arayüzü Kilitle
+                btnIndir.Enabled = false;
+                progressBar1.Value = 0;
+                progressBar1.Style = ProgressBarStyle.Marquee; // Bekleme animasyonu
+                lblDurum.Text = "Video bilgileri alınıyor...";
+
+                // İstemciyi sadece ihtiyaç duyulduğunda oluştur (Performans)
+                if (youtube == null) youtube = new YoutubeClient();
+
+                // 2. Video Bilgilerini Çek
+                var video = await youtube.Videos.GetAsync(url);
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+
+                // Sadece Ses -> En Yüksek Kalite
+                var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+
+                if (streamInfo == null)
+                {
+                    MessageBox.Show("Bu videodan ses dosyası alınamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ResetUI();
+                    return;
+                }
+
+                // 3. Dosya Yolunu Hazırla
+                // Yasaklı karakterleri temizle (Örn: Tarkan / Yolla -> Tarkan _ Yolla)
+                string temizBaslik = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+                string tamDosyaYolu = "";
+
+                // Kullanıcı "Bana Sor" dediyse sor, demediyse Masaüstüne indir
+                bool sorsunMu = (chkSor != null && chkSor.Checked);
+
+                if (sorsunMu)
+                {
+                    using (SaveFileDialog sfd = new SaveFileDialog())
+                    {
+                        sfd.FileName = temizBaslik + ".mp3";
+                        sfd.Filter = "MP3 Dosyası|*.mp3";
+                        sfd.Title = "Kaydedilecek Yeri Seçin";
+                        progressBar1.Style = ProgressBarStyle.Blocks;
+
+                        if (sfd.ShowDialog() == DialogResult.OK)
+                            tamDosyaYolu = sfd.FileName;
+                        else
+                        {
+                            lblDurum.Text = "İşlem iptal edildi.";
+                            ResetUI();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    // Otomatik Masaüstü
+                    string masaustu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    tamDosyaYolu = Path.Combine(masaustu, temizBaslik + ".mp3");
+                }
+
+                // 4. İndirme İşlemi
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                lblDurum.Text = "İndiriliyor...";
+
+                var progress = new Progress<double>(p =>
+                {
+                    progressBar1.Value = (int)(p * 100);
+                    lblDurum.Text = $"İndiriliyor... %{(int)(p * 100)}";
+                });
+
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, tamDosyaYolu, progress);
+
+                // 5. Bitiş
+                lblDurum.Text = "Tamamlandı!";
+
+                DialogResult cevap = MessageBox.Show(
+                    $"{video.Title}\nBaşarıyla indirildi!\n\nHemen çalmak ister misiniz?",
+                    "İndirme Başarılı",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Information);
+
+                if (cevap == DialogResult.Yes)
+                {
+                    // Varsayılan müzik çalar ile aç
+                    Process.Start(new ProcessStartInfo(tamDosyaYolu) { UseShellExecute = true });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu:\n{ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                lblDurum.Text = "Hata oluştu.";
+            }
+            finally
+            {
+                ResetUI();
+            }
+        }
+
+
+
+        // Arayüzü sıfırlayan yardımcı metod
+        private void ResetUI()
+        {
+            btnIndir.Enabled = true;
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            progressBar1.Value = 0;
+        }
+    }
+}
